237e656504698885910c41770915939dc14ef354
Tim Urista <timothy.urista@gmail.com>
2018-08-25 19:44:41 -0700

    minor update to the face recognition key prop for bounding boxes

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -7,7 +7,7 @@ const FaceRecognition = ({ imageUrl, boxes }) => {
       <div className='absolute mt2'>
         <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
         {boxes.map(box => (
-          <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+          <div key={box.topRow} className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
         ))}
       </div>
     </div>
