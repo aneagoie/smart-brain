d3e4a2f9d9cc12e39359db9b0a21ce2a95dcfba6
Tim Urista <timothy.urista@gmail.com>
2018-08-31 06:33:24 -0700

    add boost strap css to app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
