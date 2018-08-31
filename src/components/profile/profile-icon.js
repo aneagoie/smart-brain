c3ce5f7f716edf9cb6fd2cb9630a4da65b2f5d5f
Tim Urista <timothy.urista@gmail.com>
2018-08-31 07:08:53 -0700

    add profile dropdown toggle

--- src/components/profile/profile-icon.js
+++ src/components/profile/profile-icon.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 class ProfileIcon extends React.Component {
-  state = {
-    dropdownOpen: false,
+  constructor(props) {
+    super(props);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle = () => {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
   }
 
   render() {
 
     return (
-      <div class="pa4 tc">
-        <img
-          src="http://tachyons.io/img/logo.jpg"
-          class="br-100 ba h3 w3 dib" alt="avatar" />
+      <div className="pa4 tc">
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <DropdownToggle tag="span" aria-expanded={this.state.dropdownOpen} data-toggle="dropdown">
+            <img
+              src="http://tachyons.io/img/logo.jpg"
+              className="br-100 ba h3 w3 dib" alt="avatar" />
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem>View Profile</DropdownItem>
+            <DropdownItem>SignOut</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </div>
     )
   }
