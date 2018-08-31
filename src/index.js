387bf767e3fae06f056b436d9df8179fd6d51870
Tim Urista <timothy.urista@gmail.com>
2018-08-31 06:34:12 -0700

    change order so tachyons not overridden

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'tachyons';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'tachyons';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
