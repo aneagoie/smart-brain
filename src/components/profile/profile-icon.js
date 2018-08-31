dd8216240ea184e4ff1d63761da490ed9b8e3445
Tim Urista <timothy.urista@gmail.com>
2018-08-31 07:13:57 -0700

    update profile styling

--- src/components/profile/profile-icon.js
+++ src/components/profile/profile-icon.js
@@ -25,7 +25,7 @@ class ProfileIcon extends React.Component {
               src="http://tachyons.io/img/logo.jpg"
               className="br-100 ba h3 w3 dib" alt="avatar" />
           </DropdownToggle>
-          <DropdownMenu>
+          <DropdownMenu className='b--transparent shadow-5' style={{ marginTop: '20px', backgroundColor: 'rgba(255, 255, 255, 0.5)'}}>
             <DropdownItem>View Profile</DropdownItem>
             <DropdownItem>SignOut</DropdownItem>
           </DropdownMenu>
