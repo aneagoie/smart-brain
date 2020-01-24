b683dc099187dcafd0b4b9716d49cc9fcb757395
sensei100 <tuh42082@temple.edu>
2020-01-24 14:32:52 -0500

    Add Modal for viewing profile

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Modal.css';
+
+const modalRoot = document.getElementById('modal-root');
+
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.el = document.createElement('div');
+  }
+
+  componentDidMount() {
+    modalRoot.appendChild(this.el)
+  }
+
+  componentWillUnmount() {
+    modalRoot.removeChild(this.el)
+  }
+
+  render() {
+    return ReactDOM.createPortal(
+      this.props.children,
+      this.el
+    );
+  }
+}
+
+export default Modal;
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Profile.css';
+
+const Profile = ({ isProfileOpen, toggleModal }) => {
+  return (
+    <div
+    className="profile-modal">
+      <button onClick={toggleModal}>Click</button>
+    </div>
+  );
+}
+
+export default Profile;
