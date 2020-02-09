0cc3ad8b830cedeee5fb2b70d20c2dedad1c3f78
sensei100 <tuh42082@temple.edu>
2020-02-09 10:08:08 -0500

    Adds ranking emojis

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,46 @@
 import React from 'react';
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+class Rank extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emoji: ''
+    }
+  }
+
+  componentDidMount() {
+    this.generateEmoji(this.props.entries)
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.entries === this.props.entries && prevProps.name === this.props.name) {
+        return null
+    }
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = (entries) => {
+    fetch(`https://46ymfzdgd6.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`)
+      .then(response => response.json())
+      .then(data => this.setState({ emoji: data.imput }))
+      .catch(console.log)
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='white f3'>
+          {`${this.props.name}, your current entry count is...`}
+        </div>
+        <div className='white f1'>
+          {this.props.entries}
+        </div>
+        <div className='white f3'>
+          {`Rank Badge: ${this.state.emoji}`}
+        </div>
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Rank;
