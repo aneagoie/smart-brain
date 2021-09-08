18c7f1db22ca6a52f643f168bb25763a5ba7be00
Andrei Neagoie <aneagoie@MacBook-Pro-2.lan>
2021-09-08 05:52:08 -0400

    updates Reac 17

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # SmartBrain - v2
 Final project for ZTM course
 
+
 1. Clone this repo
 2. Run `npm install`
 3. Run `npm start`
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
-
+ 
 const particlesOptions = {
   //customize this to your liking
   particles: {
