183e508e334ecde46f3abc6f70598a395d82112f
gabypega1997 <gabypega1997@gmail.com>
2022-05-29 10:20:55 +0200

    return array whitour assigning to a variable

--- src/App.js
+++ src/App.js
@@ -54,18 +54,17 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-    const boxesResult = [];
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
-      data.outputs[0].data.regions.map((region)=>{
+    return data.outputs[0].data.regions.map((region)=>{
         const clarifaiFace = region.region_info.bounding_box;
-        boxesResult.push({
+        return{
           leftCol: clarifaiFace.left_col * width,
           topRow: clarifaiFace.top_row * height,
           rightCol: width - (clarifaiFace.right_col * width),
           bottomRow: height - (clarifaiFace.bottom_row * height)
-        })
+        }
       })
       
       // region.region_info.bounding_box;
@@ -79,7 +78,7 @@ class App extends Component {
       //   bottomRow: height - (clarifaiFace.bottom_row * height)
       // })
     
-    return boxesResult;
+    // return boxesResult;
     // const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     // const image = document.getElementById('inputimage');
     // const width = Number(image.width);
