c73056a97990077334f4c827a558cc1b39f082d1
gabypega1997 <gabypega1997@gmail.com>
2022-06-11 12:50:31 +0200

    add Modal for profile view, but just a Sample Modal

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,7 +3,7 @@ import './Profile.css'
 
 const Profile = ({isProfileOpen, toggleModal}) => {
     return (
-    <div className="br3 ba b-black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-2">
+    <div className="profile-modal">
         <button onClick={toggleModal}> Click</button>
     </div>)
 }
