0b6a3635758a97f174a4fcb9f4b7acd9269bf65e
Fernanda Hirata <122220584+fernandank@users.noreply.github.com>
2023-06-09 15:46:05 -0300

    Update App.js
    linked to the back-end

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ class App extends Component {
   onButtonSubmit = () => {
     
     this.setState({imageUrl: this.state.input});
-        fetch('http://localhost:3000/imageurl', {
+        fetch('https://backend-face-recog.onrender.com', {
           method: 'post',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -79,7 +79,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://backend-face-recog.onrender.com {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
