22c7ea25d2926c75365631c0d1097055a06457c7
Fernanda Hirata <122220584+fernandank@users.noreply.github.com>
2023-06-09 16:55:46 -0300

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -19,7 +19,7 @@ class Signin extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/signin', {
+		fetch('https://backend-face-recog.onrender.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json' },
 			body: JSON.stringify({ 
