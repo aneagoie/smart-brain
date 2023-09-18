96e52438c816b191920bd09f7d44633c704ca057
Maria Corban <121573716+cmary2023@users.noreply.github.com>
2023-09-18 20:11:26 +0100

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://smartbrain-frontend-3e26.onrender.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
