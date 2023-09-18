2507e083ddd96d58074cd565e1b259e410b9b064
Maria Corban <121573716+cmary2023@users.noreply.github.com>
2023-09-18 20:09:42 +0100

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://smartbrain-frontend-3e26.onrender.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
