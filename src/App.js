5e0d1b30cce8fbdf151f46057764075e9b2af46a
Maria Corban <121573716+cmary2023@users.noreply.github.com>
2023-09-26 19:47:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,6 +42,11 @@ class App extends Component {
     }})
   }
 
+  componentDidMount(){
+    fetch('http://localhost:3000')
+    .then(response => response.json())
+    .then(console.log)
+  }
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
